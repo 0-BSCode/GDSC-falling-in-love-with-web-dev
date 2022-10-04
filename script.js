e93526c4cbae58a2424e1295321910a221b34cf4
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-10-04 20:47:41 +0800

    Refactor

--- script.js
+++ script.js
@@ -2,51 +2,39 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
-let ticketPrice = +movieSelect.value;
+let ticketPrice = 69;
 const confirmBtn = document.querySelector(".confirm");
-
-populateUI();
-//Initial count and total set
-updateSelectedCount();
-
-// Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem("selectedMovieIndex", movieIndex);
-  localStorage.setItem("selectedMoviePrice", moviePrice);
-}
 
 //Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
-//Get Data from localStorage and populate the UI
-function populateUI() {
-  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    seats.forEach((seat, index) => {
-      if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add("selected");
-      }
-    });
-  }
-  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
-  if (selectedMovieIndex !== null) {
-    movieSelect.selectedIndex = selectedMovieIndex;
-  }
+function getSelectedSeatsIndex() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  return seatsIndex;
 }
-
-//Movie select event
-movieSelect.addEventListener("change", (e) => {
-  ticketPrice = +e.target.value;
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-});
+function getOccupiedSeatsIndex() {
+  const occupiedSeats = document.querySelectorAll(".row .seat.occupied");
+  const seatsIndex = [...occupiedSeats].map((seat) => [...seats].indexOf(seat));
+  return seatsIndex;
+}
+function occupySeats() {
+  const selectedSeats = getSelectedSeatsIndex();
+  seats.forEach((seat, index) => {
+    if (selectedSeats.indexOf(index) > -1) {
+      seat.classList.add("occupied");
+    }
+  });
+}
+function resetPage() {
+  count.innerText = "0";
+  total.innerText = "0";
+  seats.forEach((seats) => seats.classList.remove("selected"));
+}
 
 //Seat click event
 container.addEventListener("click", (e) => {
@@ -57,4 +45,9 @@ container.addEventListener("click", (e) => {
     e.target.classList.toggle("selected");
     updateSelectedCount();
   }
+});
+
+confirmBtn.addEventListener("click", () => {
+  occupySeats();
+  resetPage();
 });
